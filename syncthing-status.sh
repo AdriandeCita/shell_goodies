f02b536100a71f7bdebf83050b0c7a380c1071b6
Mykola Myslovskyi <adriandecita@gmail.com>
2021-08-11 20:15:30 +0300

    update syncthing status script

--- syncthing-status.sh
+++ syncthing-status.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if !(curl -s http://127.0.0.1:8384 | grep '<body>' > /dev/null 2>&1); then
+if !(curl -s http://127.0.0.1:8384 | grep -e '<body>' -e 'Not Authorized' > /dev/null 2>&1); then
     echo "is NOT running"
 else
     echo "IS running"
